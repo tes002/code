91a7201212dbc34d2ab3060ec874aef2ff86b7dd
tes002 <31713507+tes002@users.noreply.github.com>
2018-04-09 19:54:28 -0700

    Create Pre-Order Traversal of Binary Tree.java

---
+++ Java/Pre-Order Traversal of Binary Tree.java
@@ -0,0 +1,28 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+ 
+ //Recursive
+public class Solution {
+  public List<Integer> preOrder(TreeNode root) {
+    // Write your solution here.
+    List<Integer> preResult = new ArrayList<Integer>();
+    helper(root, preResult);
+    return preResult;
+  }
+  public void helper(TreeNode root, List<Integer> preResult){
+  	if (root == null){
+    	return;
+    }
+    preResult.add(root.key);
+    helper(root.left, preResult);
+    helper(root.right, preResult);
+  }
+}
